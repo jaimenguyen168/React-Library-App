a360e57e0e38a9518b88b2b1c5142e0e694921a4
Jaime <nguyenphuocdat168@gmail.com>
2025-01-19 02:00:04 -0500

    Form UI

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+"use client";
 
-const Page = () => {
-  return <div>Page</div>;
-};
+import React from "react";
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validation";
+
+const Page = () => (
+  <AuthForm
+    type="SIGN-IN"
+    schema={signInSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+    }}
+    onSubmit={() => {}}
+  />
+);
 
 export default Page;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+"use client";
 
-const Page = () => {
-  return <div>Page</div>;
-};
+import React from "react";
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validation";
+
+const Page = () => (
+  <AuthForm
+    type="SIGN-UP"
+    schema={signInSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+      fullname: "",
+      universityId: 0,
+      universityCard: "",
+    }}
+    onSubmit={() => {}}
+  />
+);
+
 export default Page;
