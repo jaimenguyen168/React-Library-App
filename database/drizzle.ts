1b8a2dc9d65c703bf0cf24095885da96ea93aaaf
Jaime <nguyenphuocdat168@gmail.com>
2025-01-20 21:09:47 -0500

    User schema with drizzle (sql)

---
+++ database/drizzle.ts
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import config from "@/lib/config";
+import { neon } from "@neondatabase/serverless";
+
+const sql = neon(config.env.databaseUrl);
+
+// const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
